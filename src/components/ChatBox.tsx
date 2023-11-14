19ff42fa2491a79d3d958ce6a7cff4b296fbae4d
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-11-14 15:32:37 +0000

    updated dependancy for sending messages

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -29,6 +29,8 @@ const ChatBox = (props: Props) => {
     [setAllMessages],
   );
 
+ 
+
   useEffect(() => {
     async function fetchRoomMessages() {
       const roomMessages = await RoomMessageService.getRoomMessages(roomId);
@@ -37,7 +39,7 @@ const ChatBox = (props: Props) => {
       }
     }
     fetchRoomMessages();
-  }, [roomId]);
+  }, [roomId,messageInput]);
 
   useEffect(() => {
     if (messagesRef.current && socket) {
@@ -55,44 +57,45 @@ const ChatBox = (props: Props) => {
         appendMessage(data.roomMessage);
       }
     });
-  }, [socket, appendMessage,allMessages]);
+  }, [socket, appendMessage, allMessages]);
 
+  
+    async function handleSendMessage() {
+      if (messageInput.trim() !== "") {
+        socket?.emit("sendRoomMessage", {
+          roomMessage: {
+            userId: currentUser._id,
+            userName: currentUser.userName,
+            message: messageInput,
+          },
+          roomId: roomId,
+        });
 
-
-
-  async function handleSendMessage() {
-    if (messageInput.trim() !== "") {
-      socket?.emit("sendRoomMessage", {
-        roomMessage: {
-          userId: currentUser._id,
+        const userMessageToBeSent: IUserMessage = {
+          userId: currentUser._id!,
           userName: currentUser.userName,
           message: messageInput,
-        },
-        roomId: roomId,
-      });
+          messageTime: Date.now(),
+        };
 
-      const userMessageToBeSent: IUserMessage = {
-        userId: currentUser._id!,
-        userName: currentUser.userName,
-        message: messageInput,
-        messageTime: Date.now(),
-      };
+        const roomMessage: IRoomMessage = {
+          roomId: roomId,
+          messages: [userMessageToBeSent],
+        };
 
-      const roomMessage: IRoomMessage = {
-        roomId: roomId,
-        messages: [userMessageToBeSent],
-      };
-
-      await RoomMessageService.createOrUpdateRoomMessage(roomMessage);
-      setMessageInput("");
+        await RoomMessageService.createOrUpdateRoomMessage(roomMessage);
+        setMessageInput("");
+      }
     }
-  }
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSendMessage();
-    }
-  };
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        handleSendMessage();
+      }
+    };
+
+
+
 
   const formatMessageDate = (timestamp: number) => {
     const date = new Date(timestamp);
@@ -103,17 +106,19 @@ const ChatBox = (props: Props) => {
     if (index === 0) return true;
     const currentMessage = allMessages[index];
     const previousMessage = allMessages[index - 1];
-    return currentMessage.messageTime! - previousMessage.messageTime! > 3600000; 
+    return currentMessage.messageTime! - previousMessage.messageTime! > 3600000;
   };
 
-  const shouldGroupMessages = (currentMessage: IUserMessage, previousMessage?: IUserMessage) => {
-    if (!previousMessage) return false; 
+  const shouldGroupMessages = (
+    currentMessage: IUserMessage,
+    previousMessage?: IUserMessage,
+  ) => {
+    if (!previousMessage) return false;
     return (
       currentMessage.userId === previousMessage.userId &&
-      currentMessage.messageTime! - previousMessage.messageTime! <= 20000 
+      currentMessage.messageTime! - previousMessage.messageTime! <= 20000
     );
   };
-  
 
   return (
     <Box
@@ -131,44 +136,51 @@ const ChatBox = (props: Props) => {
         p={{ base: "10px", md: "20px" }}
       >
         <ScrollToBottom>
-          {allMessages.map((am, index) =>  { 
-             const previousMessage = index > 0 ? allMessages[index - 1] : undefined;
-             const groupMessages = shouldGroupMessages(am, previousMessage);
-            
-            return (
-            <Box >
-              {shouldDisplayTimestamp(index) && (
-                <Text textAlign="center" color="#A0AEC0" mb={2} key={`timestamp-${index}`}>
-                  {formatMessageDate(am.messageTime!)}
-                </Text>
-              )}
+          {allMessages.map((am, index) => {
+            const previousMessage =
+              index > 0 ? allMessages[index - 1] : undefined;
+            const groupMessages = shouldGroupMessages(am, previousMessage);
 
-            <Text
-              fontSize={{ base: "15px", md: "20px" }}
-              mb={groupMessages ? 0.5 : 3}
-              width={{
-                base: `${Math.min(75, 20 + am.message.trim().length)}%`,
-                md: `${Math.min(30, 1 + am.message.trim().length)}%`,
-              }}
-              justifyContent="center"
-              marginLeft={am.userId === currentUser._id! ? "auto" : "0px"}
-              textAlign="left"
-              color={am.userId === currentUser._id! ? "white" : "#818181"}
-              backgroundColor={
-                am.userId === currentUser._id! ? "#fb406c" : "#F4F2F2"
-              }
-              borderRadius={
-                am.userId === currentUser._id!
-                  ? "10px 10px 0px 10px"
-                  : "10px 10px 10px 0px"
-              }
-              p={{ base: "10px" }}
-              key={index}
-            >
-              {am.message.trim()}
-            </Text>
-            </Box>
-          )})}
+            return (
+              <Box>
+                {shouldDisplayTimestamp(index) && (
+                  <Text
+                    textAlign="center"
+                    color="#A0AEC0"
+                    mb={2}
+                    key={`timestamp-${index}`}
+                  >
+                    {formatMessageDate(am.messageTime!)}
+                  </Text>
+                )}
+
+                <Text
+                  fontSize={{ base: "15px", md: "20px" }}
+                  mb={groupMessages ? 0.5 : 3}
+                  width={{
+                    base: `${Math.min(75, 20 + am.message.trim().length)}%`,
+                    md: `${Math.min(30, 1 + am.message.trim().length)}%`,
+                  }}
+                  justifyContent="center"
+                  marginLeft={am.userId === currentUser._id! ? "auto" : "0px"}
+                  textAlign="left"
+                  color={am.userId === currentUser._id! ? "white" : "#818181"}
+                  backgroundColor={
+                    am.userId === currentUser._id! ? "#fb406c" : "#F4F2F2"
+                  }
+                  borderRadius={
+                    am.userId === currentUser._id!
+                      ? "10px 10px 0px 10px"
+                      : "10px 10px 10px 0px"
+                  }
+                  p={{ base: "10px" }}
+                  key={index}
+                >
+                  {am.message.trim()}
+                </Text>
+              </Box>
+            );
+          })}
           <Box ref={messagesRef}></Box>
         </ScrollToBottom>
       </Box>
